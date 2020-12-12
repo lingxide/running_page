6668492a88379da03fbcdbdeee2aa1ccd7eff751
lingxide <hi@iam.lc>
2020-12-12 13:06:31 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -12,6 +12,20 @@ const activities = [
     "average_heartrate": null,
     "average_speed": 3.95,
     "streak": 1
+  },
+  {
+    "run_id": 9223370429079707641,
+    "name": "run from keep",
+    "distance": 173.46756,
+    "moving_time": "0:01:43",
+    "type": "Run",
+    "start_date": "2020-12-12 12:09:22",
+    "start_date_local": "2020-12-12 20:09:22",
+    "location_country": "{'latitude': 22.943631498786047, 'longitude': 113.35399341963749, 'country': '\u4e2d\u56fd', 'nationCode': '156', 'province': '\u5e7f\u4e1c\u7701', 'city': '\u5e7f\u5dde\u5e02', 'cityCode': '020'}",
+    "summary_polyline": "sl`kCcnzrTD?NEFIZEBEBEFHAJR@ZAd@PJ?H@HAD?TEDHFDJD",
+    "average_heartrate": 116.0,
+    "average_speed": 1.6841510679611649,
+    "streak": 1
   }
 ];
 
