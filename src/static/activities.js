a368c9b99f40a3710f4cf1800bf0ec1322fe05c7
lingxide <hi@iam.lc>
2020-12-12 10:40:07 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -1,3 +1,18 @@
-const activities = [];
+const activities = [
+  {
+    "run_id": 4461370932,
+    "name": "test",
+    "distance": 23.7,
+    "moving_time": "0:00:06",
+    "type": "Run",
+    "start_date": "2020-12-12 10:37:49+00:00",
+    "start_date_local": "2020-12-12 18:37:49",
+    "location_country": "\u756a\u79ba\u533a, \u5e7f\u5dde\u5e02, \u5e7f\u4e1c\u7701, 511400, China \u4e2d\u56fd",
+    "summary_polyline": "_fakC{azrTh@I",
+    "average_heartrate": null,
+    "average_speed": 3.95,
+    "streak": 1
+  }
+];
 
 export {activities};
